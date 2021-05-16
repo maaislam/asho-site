ef1246f0d345c24b2312608e9868180e16a92b2d
maaislam <maaislam@outlook.com>
2021-05-17 04:00:11 +0600

    api test

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
-const dev = process.env.NODE_ENV !== 'production';
 module.exports = {
   env: {
-    baseURL: dev ? 'http://localhost:3000' : 'https://musing-sinoussi-985e12.netlify.app',
+    baseURL: 'https://musing-sinoussi.netlify.app',
   },
 };
